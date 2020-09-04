8071bcada72ffca5c16ad357408fe766c269bac1
davidkgreenfield <davidkgreenfield@comcast.net>
2020-09-04 06:27:44 -0500

    add auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './App.css';
 import { Layout, Menu } from 'antd';
-import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
+import { AmplifyAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import logo from './logo.svg';
 import ChatContainer from './containers/ChatContainer';
 
@@ -19,23 +19,27 @@ function App() {
       default:
     }
   };
-
+  const federated = {
+    facebookAppId: '2732079550445715',
+  };
   return (
-    <div>
-      <Layout>
-        <Header>
-          <img src={logo} className="logo" width="50" height="64" alt="O" />
-          <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} onClick={handleMenuClick}>
-            <Menu.Item key="1">Chat</Menu.Item>
-            <Menu.Item key="3"><AmplifySignOut /></Menu.Item>
-          </Menu>
-        </Header>
-        <Content>
-          {content}
-        </Content>
-      </Layout>
-    </div>
+    <AmplifyAuthenticator federated={federated}>
+      <div>
+        <Layout>
+          <Header>
+            {/* <img src={logo} className="logo" width="50" height="64" alt="O" /> */}
+            <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} onClick={handleMenuClick}>
+              <Menu.Item key="1">Chat</Menu.Item>
+              <Menu.Item key="2"><AmplifySignOut /></Menu.Item>
+            </Menu>
+          </Header>
+          <Content>
+            {content}
+          </Content>
+        </Layout>
+      </div>
+    </AmplifyAuthenticator>
   );
 }
 
-export default withAuthenticator(App);
+export default App;
